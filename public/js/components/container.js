f19bbceabc12142b72a5a646e3d68e5c4688d4af
Blade Barringer <blade@crookedneighbor.com>
2016-03-18 14:22:45 -0500

    Add hello world view

---
+++ public/js/components/container.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Container = React.createClass({
+  render: function () {
+    return (
+      <div>
+        Hello World!
+      </div>
+    )
+  }
+})
+
+export default Container
