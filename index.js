9db833f5bbf54bc7e8b9d6a9a3fdb669a08404f0
Blade Barringer <blade@crookedneighbor.com>
2016-03-19 19:11:38 -0500

    Add route for user images

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 'use strict'
 
-var PORT = process.env.EXPRESS_PORT || 1337
 var express = require('express')
+var fs = require('fs')
 var app = express()
+
+const PORT = process.env.EXPRESS_PORT || 1337
+const USER_IMAGES_PATH_BASE = `${__dirname}/public/images/users`
+const DEFAULT_USER_IMAGE = `${USER_IMAGES_PATH_BASE}/default.jpg`
 
 app.use(express.static('dist'))
 app.use(express.static('public'))
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html')
+})
+
+app.get('/users/images/:name', function (req, res) {
+  let name = req.params.name
+  let imagePath = `${USER_IMAGES_PATH_BASE}/${name}.jpg`
+
+  try {
+    fs.statSync(imagePath)
+  } catch (e) {
+    imagePath = DEFAULT_USER_IMAGE
+  }
+
+  res.sendFile(imagePath)
 })
 
 app.listen(PORT, function () {
